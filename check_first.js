783cbc5fc3dfacd53c3b84de18aa46281a36aff6
Macbook <macbook@Macbooks-MacBook-Air.local>
2026-06-26 21:41:53 +0700

    chore: hapus file sampah (debug scripts + indonesia-province.json 15MB)

--- check_first.js
+++
@@ -1,4 +0,0 @@
-import fs from 'fs';
-const data = JSON.parse(fs.readFileSync('indonesia-province.json', 'utf8'));
-console.log('First feature properties:', data.features[0].properties);
-console.log('First feature id:', data.features[0].id);
--- debug_mapping.js
+++
@@ -1,4 +0,0 @@
-import fs from 'fs';
-const data = JSON.parse(fs.readFileSync('indonesia-province.json', 'utf8'));
-const list = data.features.map(f => ({ name: f.properties.Propinsi, kode: f.properties.kode, id_prop: f.properties.ID }));
-console.log(list);
